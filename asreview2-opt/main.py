8f57891af74b1bfa26e8de63f84399124469f2ee
timovdk <5330531+timovdk@users.noreply.github.com>
2024-12-17 09:26:09 +0100

    add history param and single threaded objective

--- asreview2-opt/main.py
+++ asreview2-opt/main.py
@@ -23,9 +23,12 @@ NUMBER_OF_STUDIES = 260
 # Optuna variables
 OPTUNA_TRIALS = 100
 OPTUNA_TIMEOUT = 260000  # seconds
+OPTUNA_JOBS = mp.cpu_count() - 2
+PARALLELIZE_OBJECTIVE = False
 
 # Early stopping condition variables
 MIN_TRIALS = 20
+HISTORY = 5
 STOPPING_THRESHOLD = 0.001
 
 # list of studies
@@ -37,7 +40,7 @@ studies = pd.read_json("synergy_studies_1000.jsonl", lines=True).head(
 # Function to run the loop in parallel
 def run_parallel(studies, *args, **kwargs):
     losses = []
-    with ProcessPoolExecutor(max_workers=mp.cpu_count() - 1) as executor:
+    with ProcessPoolExecutor(max_workers=mp.cpu_count() - 2) as executor:
         # Submit tasks
         futures = {
             executor.submit(process_row, row, *args, **kwargs): i
@@ -85,7 +88,7 @@ def process_row(row, alpha, ratio):
     return loss(padded_results)
 
 
-def objective(trial):
+def objective_parallel(trial):
     # Use logarithmic normal distribution for alpha (alpha effect is non-linear)
     alpha = trial.suggest_float("alpha", 1e-3, 100, log=True)
 
@@ -95,31 +98,75 @@ def objective(trial):
     return run_parallel(studies, alpha=alpha, ratio=ratio)
 
 
+def objective(trial):
+    # Use logarithmic normal distribution for alpha (alpha effect is non-linear)
+    alpha = trial.suggest_float("alpha", 1e-3, 100, log=True)
+
+    # Use normal distribution for ratio (ratio effect is linear)
+    ratio = trial.suggest_float("ratio", 0.0, 30.0)
+
+    losses = []
+    for i, row in studies.iterrows():
+        with open(PICKLE_FOLDER_PATH / f"{row['dataset_id']}.pkl", "rb") as f:
+            fm, labels = pickle.load(f)
+
+        priors = row["prior_inclusions"] + row["prior_exclusions"]
+
+        # Create classifier and balancer with optuna value
+        clf = NaiveBayesClassifier(alpha=alpha)
+        blc = Balanced(ratio=ratio)
+
+        # Setup simulation
+        simulate = asreview.Simulate(
+            fm=fm,
+            labels=labels,
+            classifier=clf,
+            balance_strategy=blc,
+            query_strategy=MaxQuery(),
+            feature_extraction=Tfidf(),
+        )
+
+        # Set priors
+        simulate.label(priors, prior=True)
+
+        # Start simulation
+        simulate.review()
+
+        # Return loss
+        padded_results = list(simulate._results["label"]) + [0] * (
+            len(simulate.labels) - len(simulate._results["label"])
+        )
+        losses.append(loss(padded_results))
+
+    return np.mean(losses)
+
+
 class StopWhenOptimumReached:
-    def __init__(self, min_trials: int, threshold: int):
+    def __init__(self, min_trials: int, threshold: int, history: int):
         self.min_trials = min_trials
         self.threshold = threshold
+        self.history = history
 
     def __call__(
         self, study: optuna.study.Study, trial: optuna.trial.FrozenTrial
     ) -> None:
         # If there are more than min_trials completed, check early stopping condition
         if trial.number > self.min_trials:
-            # Take previous min_trials trials (hence min_trials + 1)
-            prev_5_trials = study.trials[1 : self.min_trials + 1]
+            # Take history num of trials (hence history + 1)
+            prev_trials = study.trials[1 : self.history + 1]
 
-            # Calculate mean loss value of the previous min_trials trials
-            mean_value = np.mean([prev_trial.value for prev_trial in prev_5_trials])
+            # Calculate mean loss value of the prev_trials
+            mean_loss = np.mean([prev_trial.value for prev_trial in prev_trials])
 
             # If the difference is smaller than threshold, we stop the entire study
-            if abs(mean_value - trial.value) < self.threshold:
+            if abs(mean_loss - trial.value) < self.threshold:
                 study.stop()
 
 
 if __name__ == "__main__":
     sampler = optuna.samplers.TPESampler()
     study_stop_cb = StopWhenOptimumReached(
-        min_trials=MIN_TRIALS, threshold=STOPPING_THRESHOLD
+        min_trials=MIN_TRIALS, threshold=STOPPING_THRESHOLD, history=HISTORY
     )
 
     study = optuna.create_study(
@@ -128,10 +175,19 @@ if __name__ == "__main__":
         direction="minimize",
         sampler=sampler,
     )
-    study.optimize(
-        objective,
-        n_trials=OPTUNA_TRIALS,
-        timeout=OPTUNA_TIMEOUT,
-        callbacks=[study_stop_cb],
-    )
+    if PARALLELIZE_OBJECTIVE:
+        study.optimize(
+            objective_parallel,
+            n_trials=OPTUNA_TRIALS,
+            timeout=OPTUNA_TIMEOUT,
+            callbacks=[study_stop_cb],
+        )
+    else:
+        study.optimize(
+            objective,
+            n_trials=OPTUNA_TRIALS,
+            timeout=OPTUNA_TIMEOUT,
+            callbacks=[study_stop_cb],
+            n_jobs=OPTUNA_JOBS,
+        )
     print(f"Best value: {study.best_value} (params: {study.best_params})")
