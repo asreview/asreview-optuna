1a580d451607ea22dc393b7ea4c2a5431b125980
Timo van der Kuil <5330531+timovdk@users.noreply.github.com>
2024-12-17 12:04:41 +0100

    Feature matrices notebook to python file (#6)

--- asreview2-opt/README.md
+++ asreview2-opt/README.md
@@ -1,5 +1,5 @@
 # ASReview 2.x NB TfIDF Hyperparameter Optimization
-1. Install dependencies `pip3 install asreview optuna optuna-dashboard`
-2. Run the full `feature_matrices.ipynb` notebook (± 40 seconds)
+1. Install dependencies `pip3 install -r requirements.txt`
+2. Run `feature_matrices.py` (± 40 seconds)
 3. Run `main.py` to execute the Optuna trials
 4. Run `optuna-dashboard sqlite:///db.sqlite3` to see the Optuna dashboard
---
+++ asreview2-opt/feature_matrices.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pickle
+
+import synergy_dataset as sd
+import asreview as asr
+
+from asreview.models.feature_extraction import Tfidf
+
+print("synergy-dataset version", sd.__version__)
+print("asreview version:", asr.__version__)
+
+folder_pickle_files = Path("synergy-dataset", "pickles")
+folder_pickle_files.mkdir(parents=True, exist_ok=True)
+
+for dataset in sd.iter_datasets():
+    df = dataset.to_frame().reset_index()
+
+    X = Tfidf().fit_transform(
+        df["title"].fillna("").values, df["abstract"].fillna("").values
+    )
+
+    with open(folder_pickle_files / f"{dataset.name}.pkl", "wb") as f:
+        pickle.dump((X, df["label_included"]), f)
