074eff910b2c2a56670001068fa3cd502d3c2335
timovdk <5330531+timovdk@users.noreply.github.com>
2024-12-18 14:53:53 +0100

    Reduce ratio interval

--- asreview2-opt/main.py
+++ asreview2-opt/main.py
@@ -103,7 +103,7 @@ def process_row(row, params, ratio):
 
 def objective(trial):
     # Use normal distribution for ratio (ratio effect is linear)
-    ratio = trial.suggest_float("ratio", 1.0, 30.0)
+    ratio = trial.suggest_float("ratio", 1.0, 10.0)
     params = optuna_studies_params[CLASSIFIER_TYPE](trial)
 
     if PARALLELIZE_OBJECTIVE:
