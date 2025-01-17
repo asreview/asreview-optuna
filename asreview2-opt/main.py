b2d7840b7eb33133e9275a4494dcac3a05278ba0
timovdk <5330531+timovdk@users.noreply.github.com>
2025-01-17 16:18:59 +0100

    revert debug params

--- asreview2-opt/main.py
+++ asreview2-opt/main.py
@@ -19,9 +19,9 @@ from classifiers import classifier_params, classifiers
 from feature_extractors import feature_extractor_params, feature_extractors
 
 # Study variables
-VERSION = 2
+VERSION = 1
 PICKLE_FOLDER_PATH = Path("synergy-dataset", "pickles")
-N_STUDIES = 1
+N_STUDIES = 260
 CLASSIFIER_TYPE = "nb"  # Options: "nb", "log", "svm", "rf"
 FEATURE_EXTRACTOR_TYPE = "tfidf"
 PRE_PROCESSED_FMS = False  # False = on the fly
