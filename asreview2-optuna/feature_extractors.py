a4e1006582edd24bdd4333105e544a78d3342403
timovdk <5330531+timovdk@users.noreply.github.com>
2025-01-24 14:46:53 +0100

    setup full study

--- asreview2-optuna/feature_extractors.py
+++ asreview2-optuna/feature_extractors.py
@@ -12,8 +12,8 @@ def tfidf_params(trial: optuna.trial.FrozenTrial):
     min_df = trial.suggest_int("tfidf__min_df", 1, 10)
 
     # trial.suggest_categorical does not support tuples, so choose max_ngram_range first, then create a tuple.
-    max_ngram_range = trial.suggest_int("tfidf__max_ngram_range", 1, 3)
-    ngram_range = (1, max_ngram_range)
+    #max_ngram_range = trial.suggest_int("tfidf__max_ngram_range", 1, 3)
+    ngram_range = (1, 2)
 
     sublinear_tf = trial.suggest_categorical("tfidf__sublinear_tf", [True, False])
 
--- asreview2-optuna/main.py
+++ asreview2-optuna/main.py
@@ -19,8 +19,8 @@ from classifiers import classifier_params, classifiers
 from feature_extractors import feature_extractor_params, feature_extractors
 
 # Study variables
-VERSION = 2
-STUDY_SET = "demo"
+VERSION = 1
+STUDY_SET = "full"
 PICKLE_FOLDER_PATH = Path("synergy-dataset", "pickles")
 CLASSIFIER_TYPE = "rf"  # Options: "nb", "log", "svm", "rf"
 FEATURE_EXTRACTOR_TYPE = "tfidf"  # Options: "tfidf", "onehot"
