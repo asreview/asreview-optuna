c468fc9a5b9f911f37a5ea212c46afbcbeee2d2c
timovdk <5330531+timovdk@users.noreply.github.com>
2025-02-05 15:51:05 +0100

    Setup rf study with ndcg

--- asreview2-optuna/classifiers.py
+++ asreview2-optuna/classifiers.py
@@ -6,8 +6,6 @@ from asreview.models.classifiers import (
     SVM,
 )
 
-from sklearn.ensemble import RandomForestClassifier
-
 
 def naive_bayes_params(trial: optuna.trial.FrozenTrial):
     # Use logarithmic normal distribution for alpha (alpha effect is non-linear)
@@ -29,12 +27,8 @@ def svm_params(trial: optuna.trial.FrozenTrial):
 
 def random_forest_params(trial: optuna.trial.FrozenTrial):
     # Use normal distribution for n_estimators (n_estimators effect is linear)
-    n_estimators = trial.suggest_int("rf__n_estimators", 50, 200)
-
-    # Use normal distribution for max_features (max_features effect is linear)
-    max_features = trial.suggest_categorical("rf__max_features", ["sqrt", "log2"])
-
-    return {"n_estimators": n_estimators, "max_features": max_features}
+    n_estimators = trial.suggest_int("rf__n_estimators", 100, 200)
+    return {"n_estimators": n_estimators, "max_features": "sqrt"}
 
 
 classifier_params = {
@@ -43,24 +37,6 @@ classifier_params = {
     "svm": svm_params,
     "rf": random_forest_params,
 }
-
-
-class RFClassifier(RandomForestClassifier):
-    """Random forest classifier.
-
-    Based on the sklearn implementation of the random forest
-    sklearn.ensemble.RandomForestClassifier.
-    """
-
-    name = "rf"
-    label = "Random forest"
-
-    def __init__(self, n_estimators=100, max_features=10, **kwargs):
-        super().__init__(
-            n_estimators=int(n_estimators),
-            max_features=max_features,
-            **kwargs,
-        )
 
 
 classifiers = {
