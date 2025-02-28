35a177afa905c5dbc885ea508b8bce2a1b5dfdf5
timovdk <5330531+timovdk@users.noreply.github.com>
2025-02-28 11:57:25 +0100

    Setup NNs

--- asreview2-optuna/classifiers.py
+++ asreview2-optuna/classifiers.py
@@ -24,7 +24,7 @@ def logistic_params(trial: optuna.trial.FrozenTrial):
 def svm_params(trial: optuna.trial.FrozenTrial):
     # Use logarithmic normal distribution for C (C effect is non-linear)
     C = trial.suggest_float("svm__C", 0.001, 100, log=True)
-    
+
     return {"C": C, "loss": "squared_hinge"}
 
 
@@ -36,16 +36,25 @@ def random_forest_params(trial: optuna.trial.FrozenTrial):
     max_features = trial.suggest_int("rf__max_features", 2, 20)
     return {"n_estimators": n_estimators, "max_features": max_features}
 
+
 def mlp_params(trial: optuna.trial.FrozenTrial):
     alpha = trial.suggest_categorical("mlp__alpha", [1e-5, 1e-4, 1e-3, 1e-2])
     learning_rate_init = trial.suggest_categorical("mlp__lr_init", [0.001, 0.01])
-    activation = 'relu'#trial.suggest_categorical("mlp__activation", ['relu', 'tanh'])
-    solver = 'adam'#trial.suggest_categorical("mlp__solver", ['adam', 'lbfgs'])
-    batch_size = trial.suggest_categorical("mlp__batch_size", [256, 512])
+    activation = "relu" # trial.suggest_categorical("mlp__activation", ['relu', 'tanh'])
+    solver = "adam"  # trial.suggest_categorical("mlp__solver", ['adam', 'lbfgs'])
+    batch_size = "auto"  # trial.suggest_categorical("mlp__batch_size", [256, 512])
     max_iter = trial.suggest_categorical("mlp__max_iter", [200, 500, 1000])
-    early_stopping = True #trial.suggest_categorical("mlp__early_stopping", [True, False])
+    early_stopping = False # trial.suggest_categorical("mlp__early_stopping", [True, False])
 
-    return {"alpha": alpha, "learning_rate_init": learning_rate_init, "activation": activation, "solver": solver, "batch_size": batch_size, "max_iter": max_iter, "early_stopping": early_stopping}
+    return {
+        "alpha": alpha,
+        "learning_rate_init": learning_rate_init,
+        "activation": activation,
+        "solver": solver,
+        "batch_size": batch_size,
+        "max_iter": max_iter,
+        "early_stopping": early_stopping,
+    }
 
 
 classifier_params = {
@@ -55,6 +64,7 @@ classifier_params = {
     "rf": random_forest_params,
     "nn": mlp_params,
 }
+
 
 class MLP(MLPClassifier):
     """Multi Layer Perceptron classifier.
@@ -66,7 +76,15 @@ class MLP(MLPClassifier):
     name = "nn"
     label = "Multi Layer Perceptron"
 
-    def __init__(self, n_dims=1024, alpha=0.001, activation="relu", solver="adam", max_iter=200, **kwargs):
+    def __init__(
+        self,
+        n_dims=1024,
+        alpha=0.001,
+        activation="relu",
+        solver="adam",
+        max_iter=200,
+        **kwargs,
+    ):
         self.n_dims = n_dims
         hidden_layer_1 = max(self.n_dims // 4, 64)  # At least 64 neurons
         hidden_layer_2 = max(self.n_dims // 8, 32)  # At least 32 neurons
@@ -82,6 +100,7 @@ class MLP(MLPClassifier):
 
     def fit(self, X, y, sample_weight=None):
         super().fit(X, y)
+
 
 classifiers = {
     "nb": NaiveBayes,
